c15574e65c1529bbf97209f010be460c9d989cf1
anbaykova <anbaykova@gmail.com>
2017-11-20 17:44:08 +0200

    add popup

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,48 @@
-/**
- * Created by D&A on 14.11.2017.
- */
-var open = document.getElementById(#show-popup);
-var
+"use strict";
+var open = document.getElementById("show-popup");
+var close = document.getElementById("hide-popup");
+var popup = document.getElementById("wrap-login")
+console.log(open, close);
+open.addEventListener("click", function (event) {
+    popup.classList.add("open");
+});
+close.addEventListener("click", function (event) {
+    popup.classList.remove("open");
+});
+
+
+// var open = document.getElementById("show-popup");
+// var close = document.getElementById("hide-popup");
+// console.log(open, close);
+// open.addEventListener("click", function (event) {
+//     event.preventDefault();
+//
+//
+// })
+//
+//
+// var billingForm = document.getElementById('cart');
+// var requiredField = document.querySelectorAll('input[required], textarea[required]' );
+// console.log(requiredField);
+// billingForm.addEventListener('Submit', function(event) {
+//     event.preventDefault();
+//     console.log("Submit");
+//     requiredField.forEach(function (element) {
+//         if (element.value.length === 0) {
+//             console.log(element.parentNode.classList.add('has-error'))
+//         }
+//         else {
+//             element.parentNode.classList.remove('has-error');
+//             element.parentNode.classList.add('has-success');
+//         }
+//     });
+//     // var zip = document.getElementById('field-zip');
+//     // console.log(zip.value.length);
+//     //
+//     //
+//     // < 5 && zip.value.length )
+//     // if (zip.value.length < 5 && zip.value.length > 10) {
+//     //
+//     // }
+// })
+
