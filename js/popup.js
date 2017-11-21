30587694b2b5ae3b91859e63d9b6eb84aa0571cd
anbaykova <anbaykova@gmail.com>
2017-11-21 13:30:14 +0200

    fixed footer wrap and js

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,44 @@
 "use strict";
 var open = document.getElementById("show-popup");
 var close = document.getElementById("hide-popup");
-var popup = document.getElementById("wrap-login")
-console.log(open, close);
+var popup = document.getElementById("wrap-login");
+var wrapScroll;
+wrapScroll = document.body;
+wrapScroll.addEventListener("scroll", function (event) {
+
+});
 open.addEventListener("click", function (event) {
     popup.classList.toggle("open");
+    // popup.classList.add("scroll");
 });
 close.addEventListener("click", function (event) {
     popup.classList.toggle("open");
 });
 
 
+// document.body.style.overflow = 'hidden';
+// document.getElementById("wrap-login").overflow = "auto";
 
-// var open = document.getElementById("show-popup");
-// var close = document.getElementById("hide-popup");
-// console.log(open, close);
-// open.addEventListener("click", function (event) {
-//     event.preventDefault();
-//
-//
-// })
-//
-//
-// var billingForm = document.getElementById('cart');
+// document.getElementById("wrap-login").scrolling = 'yes';
+
+// function offScroll () {
+//     document.body.style.overflow = "hidden";
+//     this.onmouseout = function () {
+//         document.body.style.overflow = "auto";
+//     };
+// }
+// document.getElementById("wrap-login").onmouseover = function () {
+//     offScroll();
+// };
+
+var billingForm = document.getElementById("cart");
+var requiredFields = document.querySelectorAll("input[required]");
+
+console.log(requiredFields);
+billingForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("submit);
+});
 // var requiredField = document.querySelectorAll('input[required], textarea[required]' );
 // console.log(requiredField);
 // billingForm.addEventListener('Submit', function(event) {
@@ -37,13 +53,13 @@ close.addEventListener("click", function (event) {
 //             element.parentNode.classList.add('has-success');
 //         }
 //     });
-//     // var zip = document.getElementById('field-zip');
-//     // console.log(zip.value.length);
-//     //
-//     //
-//     // < 5 && zip.value.length )
-//     // if (zip.value.length < 5 && zip.value.length > 10) {
-//     //
-//     // }
-// })
+    // var zip = document.getElementById('field-zip');
+    // console.log(zip.value.length);
+    //
+    //
+    // < 5 && zip.value.length )
+    // if (zip.value.length < 5 && zip.value.length > 10) {
+    //
+    // }
+})
 
