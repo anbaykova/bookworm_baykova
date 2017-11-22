af27841b4d38715e0ba8f82f38f37ed1b4f27aaa
anbaykova <anbaykova@gmail.com>
2017-11-22 23:11:51 +0200

    fixed forms js

--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,106 @@
 var open = document.getElementById("show-popup");
 var close = document.getElementById("hide-popup");
 var popup = document.getElementById("wrap-login");
-var wrapScroll;
-wrapScroll = document.body;
-wrapScroll.addEventListener("scroll", function (event) {
 
-});
-open.addEventListener("click", function (event) {
-    popup.classList.toggle("open");
-    // popup.classList.add("scroll");
-});
-close.addEventListener("click", function (event) {
-    popup.classList.toggle("open");
-});
+if (open !== null) {
+    open.addEventListener("click", function (event) {
+        popup.classList.toggle("open");
+        document.body.classList.toggle("scroll");
+    });
+}
+if (close !== null) {
+    close.addEventListener("click", function (event) {
+        popup.classList.toggle("open");
+        document.body.classList.toggle("scroll");
+    });
+}
 
 
+
+
+var billingForm = document.getElementById("cart");
+var requiredFields = document.querySelectorAll("input[required], textarea[required]");
+console.log(requiredFields);
+
+billingForm.addEventListener("submit", function (event) {
+     event.preventDefault();
+     console.log("вася");
+});
+
+if(billingForm !== null){
+    billingForm.addEventListener('submit', valideteForm);
+}
+
+
+function valideteForm(event) {
+    event.preventDefault();
+    console.log("петя");
+    requiredFields.forEach(function (element) {
+        if (element.value.length === 0) {
+            console.log(element.parentNode.classList.add('has-error'));
+            // console.log(element);
+        }
+        else {
+            console.log(element.parentNode.classList.remove('has-error'));
+            console.log(element.parentNode.classList.add('has-success'))
+        }
+    })
+}
+
+    var zip = document.getElementsByClassName('form-block');
+    console.log(zip);
+
+    if (zip.value.length < 5 || zip.value.length > 10){
+        console.log("Invalit Zip Length");
+    }
+    // if(document.getElementById("phone"));
+    //     }
+    //
+
+// requiredFields.forEach(function (element) {
+//     if (element.value.length === 0) {
+//         console.log(element.parentNode.classList.add('has-error'));
+//     }
+//     else{
+//         console.log(element.parentNode.classList.remove('has-error'));
+//         console.log(element.parentNode.classList.add('has-success'))
+//     }
+// });
+// var zip = document.getElementById('field-zip');
+// console.log("Valid Zip Length");
+//
+// if (zip.value.length < 5 || zip.value.length > 10){
+//     console.log("Invalit Zip Length");
+// }
+//
+
+
+
+// requiredFields.forEach(function (element) {
+//          if (element.value.length === 0) {
+//              console.log(element.parentNode.classList.add('has-error'))
+//          }
+//          else {
+//              element.parentNode.classList.remove('has-error');
+//              element.parentNode.classList.add('has-success');
+//          }
+//      });
+
+
+    // var zip = document.getElementById('field-zip');
+    // console.log(zip.value.length);
+    //
+    //
+    // < 5 && zip.value.length )
+    // if (zip.value.length < 5 && zip.value.length > 10) {
+    //
+    // }
+// })
+// var wrapScroll;
+// wrapScroll = document.body;
+// wrapScroll.addEventListener("scroll", function (event) {
+//
+// });
 // document.body.style.overflow = 'hidden';
 // document.getElementById("wrap-login").overflow = "auto";
 
@@ -30,36 +116,3 @@ close.addEventListener("click", function (event) {
 // document.getElementById("wrap-login").onmouseover = function () {
 //     offScroll();
 // };
-
-var billingForm = document.getElementById("cart");
-var requiredFields = document.querySelectorAll("input[required]");
-
-console.log(requiredFields);
-billingForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log("submit);
-});
-// var requiredField = document.querySelectorAll('input[required], textarea[required]' );
-// console.log(requiredField);
-// billingForm.addEventListener('Submit', function(event) {
-//     event.preventDefault();
-//     console.log("Submit");
-//     requiredField.forEach(function (element) {
-//         if (element.value.length === 0) {
-//             console.log(element.parentNode.classList.add('has-error'))
-//         }
-//         else {
-//             element.parentNode.classList.remove('has-error');
-//             element.parentNode.classList.add('has-success');
-//         }
-//     });
-    // var zip = document.getElementById('field-zip');
-    // console.log(zip.value.length);
-    //
-    //
-    // < 5 && zip.value.length )
-    // if (zip.value.length < 5 && zip.value.length > 10) {
-    //
-    // }
-})
-
